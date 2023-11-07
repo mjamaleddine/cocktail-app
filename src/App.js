124fb5b3e3e032d32eb3cbebd8c5c3cc57b73e08
Mohamed Jamal Eddine <moe@enefty.app>
2023-11-07 19:07:58 +0100

    Fetch Cocktail Data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [cocktails, setCocktails] = useState([]);
+  const [ingredient, setIngredient] = useState('Gin'); // default ingredient
+
+  useEffect(() => {
+    if (!ingredient) return; // if no ingredient is specified, don't fetch
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+      .then(response => response.json())
+      .then(data => {
+        setCocktails(data.drinks);
+      })
+      .catch(error => console.log(error));
+  }, [ingredient]);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const inputIngredient = event.target.elements.ingredient.value.trim();
+    setIngredient(inputIngredient);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Meine Cocktail App</h2>
+        <form onSubmit={handleSearch}>
+          <input
+            name="ingredient"
+            type="text"
+            placeholder="Enter ingredient (e.g., Gin)"
+            defaultValue={ingredient}
+          />
+          <button type="submit">Search</button>
+        </form>
+        <div className="card-container">
+          {cocktails.map((cocktail) => (
+            <div key={cocktail.idDrink} className="card">
+              <h2>{cocktail.strDrink}</h2>
+              <img width="200px" src={cocktail.strDrinkThumb}/>
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
